7876b562c52c5c23b77a492b042d63ce893317d2
Piyush Jain Mac <piyush.jain@snssystem.com>
2024-05-11 16:50:46 +0530

    added chnages

--- src/app/[lang]/(dashboard)/pos/Summary.jsx
+++ src/app/[lang]/(dashboard)/pos/Summary.jsx
@@ -337,7 +337,7 @@ export default function Summary({ isRefreshOrderSummary, setIsRefreshOrderSummar
   })
 
   const handleCustomerSelect = customer => {
-    setSelectedCustomer(customer)
+    setSelectedCustomer(customer || {})
   }
 
   return (
