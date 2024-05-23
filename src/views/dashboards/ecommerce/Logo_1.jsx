93845db0b5bdafdfb8d1fc0568e988d3c5630050
Shubham <shubhamkumawat03282@gmail.com>
2024-05-23 15:38:35 +0530

    New Update

--- src/views/dashboards/ecommerce/Logo_1.jsx
+++ src/views/dashboards/ecommerce/Logo_1.jsx
@@ -2,7 +2,7 @@ const Logo_1 = props => {
   return (
     <img src='/images/logos/gold.png' style={{ width: '30%', borderRadius: '50%' }} />
 
-    //  <img src='../../../public/images/logos/pos.jpg' />
+    
   )
 }
 
